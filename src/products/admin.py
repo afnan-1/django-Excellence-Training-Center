24a4a4e3a104760f6adaaad103e73287dd16e6bf
afnan-1 <roman.nadeem@gmail.com>
2021-01-02 22:42:21 +0500

    django 3 done

--- src/products/admin.py
+++ src/products/admin.py
@@ -5,7 +5,7 @@ from .models import Product
 class ProductAdmin(admin.ModelAdmin):
 	search_fields=('slug','title','Price')
 	list_display = ['__str__','slug','Price',]
-admin.site.site_header = "Excellence Training Center "
+admin.site.site_header = "Excellence Training Center"
 admin.site.site_title = "Excellence Training Center"
 admin.site.index_title = "Welcome to Excellence Training Center"
 admin.site.register(Product, ProductAdmin)
